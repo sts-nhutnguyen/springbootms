d13c4fe2244539cd66180c6d3c49a0dcbd4001bc
nhut.nguyen <nhut.nguyen@saigontechnology.com>
2022-08-08 16:15:16 +0700

    service registry

--- department-service/src/main/java/com/msa/department/DepartmentApplication.java
+++ department-service/src/main/java/com/msa/department/DepartmentApplication.java
@@ -2,10 +2,12 @@ package com.msa.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentApplication{
 
     public static void main(String[] args){
